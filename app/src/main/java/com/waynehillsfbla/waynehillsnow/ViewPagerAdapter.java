5861539dd0dd81a5e6184da548704b7819bb6220
KPrabs106 <prabhukartik98@gmail.com>
2015-03-13 17:26:13 -0400

    Added missing files

---
+++ app/src/main/java/com/waynehillsfbla/waynehillsnow/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.waynehillsfbla.waynehillsnow;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Kartik on 3/10/2015.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    CharSequence Titles[];
+    int numTabs;
+
+    public ViewPagerAdapter(FragmentManager fm, CharSequence Titles[], int numTabs) {
+        super(fm);
+        this.Titles = Titles;
+        this.numTabs = numTabs;
+    }
+
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            CalendarTab calendarTab = new CalendarTab();
+            return calendarTab;
+        } else {
+            EventListTab eventListTab = new EventListTab();
+            return eventListTab;
+        }
+    }
+
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    public int getCount() {
+        return numTabs;
+    }
+}
