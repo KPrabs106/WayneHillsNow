cc32841b5e484e937378fd359bcd481cf789f87f
KPrabs106 <prabhukartik98@gmail.com>
2015-04-08 15:25:07 -0400

    Started creating widget

---
+++ app/src/main/java/com/waynehillsfbla/waynehillsnow/StackWidgetService.java
@@ -0,0 +1,86 @@
+package com.waynehillsfbla.waynehillsnow;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackWidgetService extends RemoteViewsService {
+
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StackRemoteViewsFactory(this.getApplicationContext(), intent);
+    }
+
+    class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+        private static final int mCount = 10;
+        private List<WidgetItem> mWidgetItems = new ArrayList<WidgetItem>();
+        private Context mContext;
+        private int mAppWidgetId;
+
+        public StackRemoteViewsFactory(Context context, Intent intent) {
+            mContext = context;
+            mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        @Override
+        public void onCreate() {
+            for (int i = 0; i < mCount; i++) {
+                mWidgetItems.add(new WidgetItem(i + "!"));
+            }
+        }
+
+        @Override
+        public void onDataSetChanged() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+            mWidgetItems.clear();
+        }
+
+        @Override
+        public int getCount() {
+            return mCount;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
+            rv.setTextViewText(R.id.widget_item, mWidgetItems.get(position).text);
+
+            Bundle extras = new Bundle();
+            extras.putInt(StackWidgetProvider.EXTRA_ITEM, position);
+            Intent fillInIntent = new Intent();
+            fillInIntent.putExtras(extras);
+            rv.setOnClickFillInIntent(R.id.widget_item, fillInIntent);
+
+            return rv;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/waynehillsfbla/waynehillsnow/WidgetItem.java
@@ -0,0 +1,12 @@
+package com.waynehillsfbla.waynehillsnow;
+
+/**
+ * Created by Kartik on 4/8/2015.
+ */
+public class WidgetItem {
+    public String text;
+
+    public WidgetItem(String text) {
+        this.text = text;
+    }
+}
